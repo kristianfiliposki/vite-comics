b3e82fd807f428dc164fdaf08715524a30ea2379
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-30 09:34:07 +0100

    i try to add the cards in a separetes components

---
+++ src/components/AppCards.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    props: ["boxes"],
+}
+import boxes from '../App.vue'
+
+</script>
+<template>
+    <div id="container">
+        <div class="WrapperBox">
+            <div v-for="element in boxes " class="box">
+                {{ element.series }}
+                <div class="foto">
+                    <img :src="element.thumb" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style></style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,34 +1,66 @@
 <script>
 export default {
+    props: ["boxes"],
     data() {
         return {
-            contenuto: "--> Content goes here <-- ",
         }
     },
     mounted() { }
 }
+import boxes from '../App.vue'
 </script>
 <template>
     <div id="container">
-        <div>
-            {{ contenuto }}
+        <div class="WrapperBox">
+            <div v-for="element in boxes " class="box">
+                {{ element.series }}
+                <div class="foto">
+                    <img :src="element.thumb" alt="">
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <style scoped>
+* {
+    box-sizing: border-box;
+}
+
 #container {
-    height: 15vh;
     background-color: #1c1c1c;
     color: white;
     font-weight: 800;
     display: flex;
     justify-content: center;
     align-items: center;
+    font-size: 0.7em;
+
 }
 
-#container div {
+#container>div {
     font-family: sans-serif;
-    width: 80%;
 
+}
+
+.WrapperBox {
+    display: flex;
+    width: 75%;
+    height: 70%;
+    flex-wrap: wrap;
+}
+
+.box {
+    margin: 2em;
+    width: calc((100% / 6) - 2em);
+}
+
+.foto {
+    width: 100%;
+    aspect-ratio: 1;
+}
+
+.foto img {
+    width: 100%;
+    height: 100%;
 }
 </style>
